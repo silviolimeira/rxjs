eb97c1f3d607beb75f973e4f082b5c580b14b8ec
Silvio Limeira <silvio.limeira@live.com>
2020-03-21 11:45:57 -0300

    Example Create Observable and subscribe test

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,14 @@
-import * as Rx from "rxjs/Observable";
+import { Observable } from "rxjs";
 
-console.log(Rx);
+var observable = Observable.create((observer: any) => {
+  observer.next("Hello everyone!");
+});
+
+observable.subscribe((x: any) => addItem(x));
+
+function addItem(val: any) {
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById("output").appendChild(node);
+}
