b5c40fc4803ae66c876129bbf268e4bfdd3e401d
Silvio Limeira <silvio.limeira@live.com>
2020-03-21 19:15:36 -0300

    Example Observable with the operator skikUntil

--- src/code.ts
+++ src/code.ts
@@ -1,13 +1,24 @@
-import { Observable, from } from "rxjs";
-import "rxjs/add/operator/pluck";
+import { Observable } from "rxjs";
+import { Subject } from "rxjs/Subject";
+import { interval } from "rxjs/Observable/interval";
+import "rxjs/add/operator/skipUntil";
 
-from([
-  { first: "Gary", last: "Simon", age: 34 },
-  { first: "Jane", last: "Simon", age: 33 },
-  { first: "John", last: "Simon", age: 30 }
-])
-  .pluck("first")
-  .subscribe((x: any) => addItem(x));
+var observable1 = Observable.create((data: any) => {
+  var i = 1;
+  setInterval(() => {
+    data.next(i++);
+  }, 1000);
+});
+
+var observable2 = new Subject();
+
+setTimeout(() => {
+  observable2.next("Hey!");
+}, 3000);
+
+var newObs = observable1.skipUntil(observable2);
+
+newObs.subscribe((x: any) => addItem(x));
 
 function addItem(val: any) {
   var node = document.createElement("li");
