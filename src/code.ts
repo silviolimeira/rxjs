26f0771da856ec6d2b522af50719823f5f768cf6
Silvio Limeira <silvio.limeira@live.com>
2020-03-22 12:56:18 -0300

    RxJS operators like map() or throttleTime

--- src/code.ts
+++ src/code.ts
@@ -1,31 +1,10 @@
-// import { fromEvent, of, forkJoin, timer } from "rxjs";
-// import { throttleTime, map } from "rxjs/operators";
-// import "rxjs/add/operator/map";
-// import "rxjs/add/observable/fromEvent";
-
-// var button = document.querySelector("button");
-
-// fromEvent(button, "click")
-//   .pipe(throttleTime(500))
-//   .map((data: MouseEvent) => {
-//     return data.clientX;
-//   })
-//   .subscribe(coordinate => {
-//     console.log(coordinate);
-//     addItem(coordinate);
-//   });
-
-// function addItem(val: any) {
-//   var node = document.createElement("li");
-//   var textnode = document.createTextNode(val);
-//   node.appendChild(textnode);
-//   document.getElementById("output").appendChild(node);
-// }
-
 import { fromEvent, of, forkJoin, timer, Observable } from "rxjs";
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/throttleTime";
+import "rxjs/add/observable/interval";
 
 var button = document.querySelector("button");
+var observable = Observable.interval(1000);
 
 var observer = {
   next: function(value: any) {
@@ -42,27 +21,12 @@ var observer = {
   }
 };
 
-// fromEvent(button, "click").subscribe(observer);
-
-var subscription = Observable.create(function(obs: any) {
-  // example 1
-  // obs.next("A value* " + Date.now().toString());
-  // obs.next("A scond value.");
-  // //obs.error("Error");
-  // setTimeout(function() {
-  //   obs.complete();
-  // }, 2000);
-  // obs.next("A third value.");
-
-  // example 2
-  button.onclick = function(event) {
-    obs.next(event);
-  };
-}).subscribe(observer);
-
-setTimeout(function() {
-  subscription.unsubscribe();
-}, 3000);
+observable
+  .map(function(value) {
+    return value * 2;
+  })
+  .throttleTime(2000) // skip values during the throttle time
+  .subscribe(observer);
 
 function addItem(val: any) {
   var node = document.createElement("li");
