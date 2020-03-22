95284b23032e81802b665767bbe4dcc08a412c25
Silvio Limeira <silvio.limeira@live.com>
2020-03-22 12:34:11 -0300

    Observables, Observers & Subscription, unsubscribe after timeout

--- src/code.ts
+++ src/code.ts
@@ -22,25 +22,47 @@
 //   document.getElementById("output").appendChild(node);
 // }
 
-import { fromEvent, of, forkJoin, timer } from "rxjs";
+import { fromEvent, of, forkJoin, timer, Observable } from "rxjs";
 import "rxjs/add/operator/map";
 
 var button = document.querySelector("button");
 
 var observer = {
-  next: function(value: MouseEvent) {
-    console.log(value.clientX);
-    addItem(value.clientX);
+  next: function(value: any) {
+    console.log(value);
+    addItem(value);
   },
   error: function(error: any) {
     console.log(error);
+    addItem(error);
   },
   complete: function() {
     console.log("Completed");
+    addItem("Completed");
   }
 };
 
-fromEvent(button, "click").subscribe(observer);
+// fromEvent(button, "click").subscribe(observer);
+
+var subscription = Observable.create(function(obs: any) {
+  // example 1
+  // obs.next("A value* " + Date.now().toString());
+  // obs.next("A scond value.");
+  // //obs.error("Error");
+  // setTimeout(function() {
+  //   obs.complete();
+  // }, 2000);
+  // obs.next("A third value.");
+
+  // example 2
+  button.onclick = function(event) {
+    obs.next(event);
+  };
+}).subscribe(observer);
+
+setTimeout(function() {
+  subscription.unsubscribe();
+}, 3000);
 
 function addItem(val: any) {
   var node = document.createElement("li");
