316e8acf580f0f07d680f4e9fa7624686913b1aa
Silvio Limeira <silvio.limeira@live.com>
2020-03-27 18:36:20 -0300

    Using Keyboard Event Listener to Check for Key Presses

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,17 @@
+// Detecting Button Press
 var buttons = document.querySelectorAll(".drum");
 
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function() {
     var letter = this.innerHTML;
 
-    switch (letter) {
-      case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case "a":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-      case "s":
-        var tom2 = new Audio("sounds/tom-3.mp3");
-        tom2.play();
-        break;
+    makeSound(letter);
 
-      default:
-    }
     console.log("letra: ", letter);
   });
 }
 
+// Object and methods example
 function HouseKeeper(yearsOfExperience, name, cleaningReportoire) {
   this.yearsOfExperience = yearsOfExperience;
   this.name = name;
@@ -36,3 +24,29 @@ function HouseKeeper(yearsOfExperience, name, cleaningReportoire) {
 var houseKeeper1 = new HouseKeeper(12, "James", ["bedroom"]);
 
 houseKeeper1.clean();
+
+// Detecting Keyboard Press
+document.addEventListener("keypress", function(event) {
+  //   this.alert("Key was pressed!");
+  console.log(event.key);
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom2 = new Audio("sounds/tom-3.mp3");
+      tom2.play();
+      break;
+
+    default:
+  }
+}
