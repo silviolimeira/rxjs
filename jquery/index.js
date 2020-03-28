94fe4aee111d4fa4c6cca53443f08502813852a3
Silvio Limeira <silvio.limeira@live.com>
2020-03-28 08:30:05 -0300

    How to Incorporate jQuery into Websites

---
+++ jquery/index.js
@@ -0,0 +1,7 @@
+// // should be used if you put javascript jquery and other scripts at head in the html
+// $(document).ready(function() {
+//   $("h1").css("color", "red");
+// });
+
+// Put in the end of body, not need of verifycation like it
+$("h1").css("color", "red");
