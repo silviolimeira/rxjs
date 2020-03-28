3f914f66c13dfb4b32114a22a42ca0d69bb4b0d4
Silvio Limeira <silvio.limeira@live.com>
2020-03-28 08:57:05 -0300

    Manipulating Styles with jQuery

--- jquery/index.js
+++ jquery/index.js
@@ -4,7 +4,24 @@
 // });
 
 // Put in the end of body, not need of verifycation like it
-$("h1").css("color", "red");
+$("h1").css("color", "red"); // 32px default
+console.log($("h1").css("font-size"));
+$("h1").css("font-size", "5rem"); //80px
+console.log($("h1").css("font-size"));
 
 // Select all the buttons on the document
 $("button");
+
+// Manipulating Styles with jQuery
+$("h1").addClass("big-title margin-50");
+
+// Remove Styles with jQuery
+function removeStyle() {
+  setTimeout(function() {
+    $("h1").removeClass("big-title");
+  }, 5000);
+}
+removeStyle();
+
+// Test if the tag has a class
+console.log($("h1").hasClass("margin-50"));
